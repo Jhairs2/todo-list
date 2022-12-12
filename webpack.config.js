ea34a8dd5c01abe07ea273b3e9c0fda4e890962b
Justin Hairston <jhairst2@vols.utk.edu>
2022-12-11 23:37:12 -0500

    Add config file

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development', 
+    
+    entry: {
+        main: path.resolve(__dirname, 'src/index.js'),
+    
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name][contenthash].js',
+    },
+    
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/template.html',
+        }),
+    ]
+}
+
